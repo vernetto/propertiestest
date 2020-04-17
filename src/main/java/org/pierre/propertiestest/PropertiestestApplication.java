88c322c78738209f35e431d961dc71a62cbfc5f8
vernetto <publicpierre@gmail.com>
2020-04-17 13:02:23 +0200

    first commit

--- src/main/java/org/pierre/propertiestest/PropertiestestApplication.java
+++ src/main/java/org/pierre/propertiestest/PropertiestestApplication.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class PropertiestestApplication implements CommandLineRunner {
 
